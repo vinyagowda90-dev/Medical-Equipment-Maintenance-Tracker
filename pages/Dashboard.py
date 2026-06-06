611d45b2d55f851dc8c30a4172111f6fbf6620e9
Vinya H G <vinyagowda90@gmail.com>
2026-06-06 09:49:18 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,1 +1,30 @@
+import streamlit as st
+from utils.data_loader import load_data
 
+df = load_data()
+
+st.title("📊 Dashboard")
+
+col1,col2,col3,col4 = st.columns(4)
+
+col1.metric(
+    "Total Devices",
+    len(df)
+)
+
+col2.metric(
+    "Avg Maintenance Cost",
+    f"${df['Maintenance_Cost'].mean():,.0f}"
+)
+
+col3.metric(
+    "Avg Downtime",
+    f"{df['Downtime'].mean():.1f} hrs"
+)
+
+col4.metric(
+    "Total Failures",
+    int(df['Failure_Event_Count'].sum())
+)
+
+st.dataframe(df.head())
