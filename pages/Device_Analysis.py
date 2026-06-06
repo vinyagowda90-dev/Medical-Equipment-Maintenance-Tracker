e0ae742779b89ddb296cf533bb2f22e48187440c
Vinya H G <vinyagowda90@gmail.com>
2026-06-06 09:49:50 +0530

    Update Device_Analysis.py

--- pages/Device_Analysis.py
+++ pages/Device_Analysis.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import plotly.express as px
+from utils.data_loader import load_data
 
+df = load_data()
+
+st.title("🔬 Device Analysis")
+
+fig = px.histogram(
+    df,
+    x="Device_Type",
+    color="Device_Type"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+fig2 = px.box(
+    df,
+    x="Device_Type",
+    y="Age"
+)
+
+st.plotly_chart(fig2,use_container_width=True)
