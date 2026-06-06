0905fe2d0a8ff6e6f355ce32a1d89270299762d6
Vinya H G <vinyagowda90@gmail.com>
2026-06-06 09:47:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Medical Device Analytics",
+    page_icon="🏥",
+    layout="wide"
+)
+
+st.title("🏥 Medical Device Failure Analytics")
+
+st.markdown("""
+### Smart Healthcare Equipment Monitoring Dashboard
+
+Analyze:
+- Device Failures
+- Maintenance Costs
+- Downtime
+- Device Age
+- Failure Trends
+- Predictive Maintenance
+""")
+
+st.image(
+    "https://images.unsplash.com/photo-1576091160550-2173dba999ef",
+    use_container_width=True
+)
