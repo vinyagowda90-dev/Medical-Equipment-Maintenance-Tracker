c3d0e239fbad7d942cff6d3482b9d630a95baa99
Vinya H G <vinyagowda90@gmail.com>
2026-06-06 10:12:31 +0530

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,61 +1,16 @@
 import pandas as pd
 import streamlit as st
 
-
 @st.cache_data
 def load_data():
-    """
-    Load Medical Device Failure Dataset
-    """
 
-    df = pd.read_csv(
-        "data/Medical_Device_Failure_dataset.csv"
-    )
+    try:
+        df = pd.read_csv(
+            "data/Medical_Device_Failure_dataset.csv"
+        )
 
-    return df
+        return df
 
-
-@st.cache_data
-def get_basic_stats(df):
-
-    stats = {
-        "total_records": len(df),
-        "total_failures": df["Failure_Event_Count"].sum(),
-        "avg_cost": round(df["Maintenance_Cost"].mean(), 2),
-        "avg_downtime": round(df["Downtime"].mean(), 2)
-    }
-
-    return stats
-
-
-@st.cache_data
-def get_device_types(df):
-
-    return sorted(
-        df["Device_Type"].unique().tolist()
-    )
-
-
-@st.cache_data
-def get_countries(df):
-
-    return sorted(
-        df["Country"].unique().tolist()
-    )
-
-
-def filter_data(df, device_type=None, country=None):
-
-    filtered_df = df.copy()
-
-    if device_type and device_type != "All":
-        filtered_df = filtered_df[
-            filtered_df["Device_Type"] == device_type
-        ]
-
-    if country and country != "All":
-        filtered_df = filtered_df[
-            filtered_df["Country"] == country
-        ]
-
-    return filtered_df
+    except Exception as e:
+        st.error(f"Dataset Error: {e}")
+        st.stop()
