080a76245b9cc9331a80bc70f5e84acf8109d05f
Vinya H G <vinyagowda90@gmail.com>
2026-06-06 09:50:56 +0530

    Update Failure_Insights.py

--- pages/Failure_Insights.py
+++ pages/Failure_Insights.py
@@ -1,1 +1,35 @@
+import streamlit as st
+import plotly.express as px
+from utils.data_loader import load_data
 
+df = load_data()
+
+st.title("⚠️ Failure Insights")
+
+fig = px.histogram(
+    df,
+    x="Failure_Event_Count",
+    color="Maintenance_Class"
+)
+
+st.plotly_chart(fig,use_container_width=True)
+
+fig2 = px.sunburst(
+    df,
+    path=[
+        "Country",
+        "Device_Type",
+        "Maintenance_Class"
+    ]
+)
+
+st.plotly_chart(fig2,use_container_width=True)
+
+st.subheader("Top Risk Devices")
+
+risk = df.sort_values(
+    "Failure_Event_Count",
+    ascending=False
+)
+
+st.dataframe(risk.head(20))
